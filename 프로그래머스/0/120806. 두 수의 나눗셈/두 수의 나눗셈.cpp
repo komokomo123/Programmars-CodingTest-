8654cca7c094951c3bdd0a4ee40415bd4df2c0b1
코모 <sjm7829@naver.com>
2025-05-30 09:30:49 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    double answer = static_cast<double>(num1) / num2;
+    answer *= 1000;
+    return static_cast<int>(answer);
+}
