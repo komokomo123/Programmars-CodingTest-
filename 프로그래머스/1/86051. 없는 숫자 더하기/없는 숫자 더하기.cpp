1799934259bbec310ee269bafd28a29cb910861b
코모 <sjm7829@naver.com>
2025-07-30 09:13:21 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 4 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 0;
+
+    for (int i = 0; i <= 9; i++) {
+        bool found = false;
+
+        for (int j = 0; j < numbers.size(); j++) {
+            if (numbers[j] == i) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            answer += i;
+        }
+    }
+    return answer;
+}
